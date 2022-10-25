850eb8c330abee9643565d9cd50ce10dbbc99cca
KrisjanisP <krisjanispetrucena@gmail.com>
2022-10-25 07:59:23 +0300

    add writing to driver

--- kernelspace/c-driver.c
+++ kernelspace/c-driver.c
@@ -10,20 +10,24 @@
 #include <linux/device.h> 
 #include <linux/uio.h>
 
+#define QRNG_DEBUG
+
 #define DEVICE_NAME "qrandom0"
 #define BUFF_BLOCK  100 // requested byte count from qrng.lumii.lv
-#define BLOCK_CNT 2
+#define BLOCK_CNT 4
 #define BUFF_TOTAL (BUFF_BLOCK*BLOCK_CNT)
 
 static u8 qrng_buffer[BUFF_TOTAL];
 static bool buff_is_renewed[BLOCK_CNT];
 static int  buff_read_it; // iterates [0;BUFF_TOTAL)
 static int  block_write_it; // iterates blocks [0:BLOCK_CNT)
-static int  buff_unrenewed_cnt = BLOCK_CNT;
+static int  buff_ready_cnt = 0;
 
 static bool qrng_ready(void) {
+    int buff_id;
+
     if(buff_read_it%BUFF_BLOCK) return true;
-    int buff_id = buff_read_it/BUFF_BLOCK;
+    buff_id = buff_read_it/BUFF_BLOCK;
     return buff_is_renewed[buff_id];
 }
 
@@ -31,7 +35,7 @@ static ssize_t get_random_byte(u8* b) {
     int buff_id = buff_read_it/BUFF_BLOCK;
     if(buff_read_it%BUFF_BLOCK==0) {
         if(!buff_is_renewed[buff_id]) return 0;
-        buff_unrenewed_cnt--;
+        buff_ready_cnt--;
         buff_is_renewed[buff_id] = false;
     }
     *b = qrng_buffer[buff_read_it];
@@ -60,7 +64,7 @@ static ssize_t get_random_bytes_qrng(struct iov_iter* iter) {
 static ssize_t qrng_read_iter(struct kiocb*, struct iov_iter*);
 static ssize_t qrng_write_iter(struct kiocb*, struct iov_iter*);
 static __poll_t qrng_poll(struct file*, poll_table*);
-static long qrng_ioctl(struct file*, unsigned int cmd, unsigned long arg);
+// static long qrng_ioctl(struct file*, unsigned int cmd, unsigned long arg);
 static int qrng_fasync(int, struct file*, int);
 
 const struct file_operations qrng_fops = {
@@ -110,34 +114,54 @@ static void __exit qrng_exit(void) {
 
 
 static ssize_t qrng_read_iter(struct kiocb* kiocb, struct iov_iter* iter) {
+    #ifdef QRNG_DEBUG
+        pr_info("QRNG: read_iter function called\n");
+    #endif
     if(!qrng_ready())
         return -EAGAIN;
 
     return get_random_bytes_qrng(iter);
 }
 
-static ssize_t qrng_write_iter(struct kiocb*, struct iov_iter*) {
+static ssize_t qrng_write_iter(struct kiocb*, struct iov_iter* iter) {
+
     ssize_t ret = 0;
     size_t copied;
 
     if(unlikely(!iov_iter_count(iter)))
         return 0;
 
-    if(buff_unrenewed_cnt==0) return -EFAULT;
-    
-    while(iov_iter_count(iter)>=BUFF_BLOCK) {
-        copied = copy_from_iter(qrng_buffer[block_write_it*BLOCK_SIZE], sizeof(block), iter);
-        block_write_it = (block_write_it+1)%BLOCK_CNT;
-        ret += copied;
+    #ifdef QRNG_DEBUG
+        pr_info("QRNG: write_iter function called\n");
+        pr_info("QRNG: write_iter iov_iter_count %d\n",iov_iter_count(iter));
+    #endif
+
+    while(iov_iter_count(iter)>=BUFF_BLOCK&&buff_ready_cnt<BLOCK_CNT) {
+        size_t block_sz = sizeof(qrng_buffer[0])*BLOCK_SIZE;
+        void* block_addr = &qrng_buffer[BLOCK_SIZE*block_write_it];
+        copied = copy_from_iter(block_addr, block_sz, iter);
+        if(copied==BUFF_BLOCK)
+        {
+            ret += copied;
+            buff_is_renewed[block_write_it] = 1;
+            block_write_it = (block_write_it+1)%BLOCK_CNT;
+            buff_ready_cnt++;
+        }
     }
+    #ifdef QRNG_DEBUG
+        pr_info("QRNG: write_iter ret %d\n",ret);
+    #endif
     
     return ret ? ret : -EFAULT;
 }
 
 static __poll_t qrng_poll(struct file* file, poll_table* wait) {
-    __poll_t res = 0
-    if(buff_unrenewed_cnt<BUFF_BLOCK) res |= EPOLLOUT;
-    if(qrng_ready()) res |= POLLIN|EPOLLRDNORM;
+    #ifdef QRNG_DEBUG
+        pr_info("QRNG: poll function called\n");
+    #endif
+    __poll_t res = 0;
+    if(buff_ready_cnt<BUFF_BLOCK) res |= EPOLLOUT;  // writing is now possible
+    if(qrng_ready()) res |= POLLIN|EPOLLRDNORM;     // there is data to read.
     return res;
 }
 
--- userspace/src/main.cpp
+++ userspace/src/main.cpp
@@ -1,20 +1,55 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <poll.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+
 #include "prng.hpp"
 #include "qrng.hpp"
+
+#define DEVICE_PATH "/dev/qrandom0"
+#define BUFF_BLOCK  100 // must match the value defined in kernelspace
 
 RNG* rng; // random num gen used to write /dev/qrandom0
 
 int main(int argc, char **argv) {
     rng = new PRNG(); // can be swapped out for QRNG
 
-    char b_arr[1000];
+    char b_arr[10];
     int b_cnt = sizeof(b_arr)/sizeof(b_arr[0]);
     rng->fetch_bytes(b_arr,b_cnt);
 
     for(int i=0;i<b_cnt;i++)
         printf("%d ", static_cast<unsigned char>(b_arr[i]));
+    printf("\n");
 
+    int fd = open(DEVICE_PATH, O_RDWR);
+    if(fd<0) {
+        if(errno == 13) printf("%s failed with permission denied");
+        else printf("%s failed with errno %d\n", DEVICE_PATH, errno);
+        return errno;
+    }
+
+    pollfd qrngpoll = {};
+    qrngpoll.fd = fd;
+    qrngpoll.events = POLLOUT;
+
+    while(1) {
+        poll(&qrngpoll,1,1000);
+        if(qrngpoll.revents & POLLOUT) {
+            qrngpoll.revents = 0;
+            printf("%s is ready for writing\n", DEVICE_PATH);
+            unsigned char arr[BUFF_BLOCK];
+            for(int i=0;i<BUFF_BLOCK;i++) arr[i] = i;
+            int wres = write(fd,arr,sizeof(arr));
+            if(wres<0) {
+                printf("write failed with errno %d\n", errno);
+                return errno;
+            }
+        } else {
+            printf("poll timed out\n");
+        }
+    }
     return 0;
 }
